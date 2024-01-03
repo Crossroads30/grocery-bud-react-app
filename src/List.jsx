71638ae7c9c55a74b57af495839e42eb7072f4bb
crossroads30 <crossroads30@mail.ru>
2024-01-03 13:05:43 +0300

    refactor: move structure of list item to separate component

--- src/List.jsx
+++ src/List.jsx
@@ -1,30 +1,12 @@
-import React from 'react'
-import { FaEdit, FaTrash } from 'react-icons/fa'
+import GroceryItem from './GroceryItem'
+
 const List = ({ list, removeItem, editItem }) => {
 	return (
 		<div className='grocery-list'>
 			{list.map(item => {
 				const { id, title } = item
 				return (
-					<article key={id} className='grocery-item'>
-						<p className='title'>{title}</p>
-						<div className='btn-container'>
-							<button
-								onClick={() => editItem(id)}
-								className='edit-btn'
-								type='button'
-							>
-								<FaEdit />
-							</button>
-							<button
-								onClick={() => removeItem(id)}
-								className='delete-btn'
-								type='button'
-							>
-								<FaTrash />
-							</button>
-						</div>
-					</article>
+					<GroceryItem key={id} id={id} title={title} editItem={editItem} removeItem={removeItem}  />
 				)
 			})}
 		</div>
