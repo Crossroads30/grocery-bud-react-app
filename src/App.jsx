6e7a36e270804560446a604cd3cc80104029a311
crossroads30 <crossroads30@mail.ru>
2024-01-03 13:15:08 +0300

    refactor: discard all prev changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import List from './List'
 import Alert from './Alert'
-import { clearList, editItem, getLocalStorage, removeItem } from './utils'
+
+ const getLocalStorage = () => {
+		let list = localStorage.getItem('list')
+		if (list) {
+			return JSON.parse(localStorage.getItem('list'))
+		} else {
+			return []
+		}
+ }
 
 function App() {
 	const [name, setName] = useState('')
@@ -40,6 +48,30 @@ function App() {
 		}
 		setName('')
 	}
+
+ const showAlert = (show = false, msg = '', type = '') => {
+		setAlert({ show, msg, type }) //(ES6 feature) if value is equal to param we just can skip this construction: 'show: show, msg: msg, type: type', and pass only one word
+ }
+
+ const clearList = () => {
+		showAlert(true, 'список очищен!', 'danger')
+		setList([])
+ }
+
+ const removeItem = id => {
+		const newList = list.filter(item => item.id !== id)
+		showAlert(true, 'продукт удален!', 'danger')
+		// setList(list.filter(item => item.id !== id))
+		setList(newList)
+ }
+
+ const editItem = id => {
+		const editingItem = list.find(item => item.id === id)
+		setIsEditing(true)
+		setEditId(id)
+		setName(editingItem.title)
+ }
+
 
   useEffect(() => {
     localStorage.setItem('list', JSON.stringify([
